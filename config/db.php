b31b9ae464182056e5d76240f78759000ce76ed5
Grigoriy <gregorct20@gmail.com>
2026-04-13 14:20:53 +0200

    Archivo de conexion a la base de datos

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+// Este archivo se encarga de realizar la conexión a la base de datos
+// Incluimos el archivo de configuración para obtener las constantes de conexión
+include 'config.php';
+
+// Hacemos referencia a las variables del archivo config.php para establecer la conexión
+$conexion = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if(!$conexion){
+    die("Error en la conexión!");
+}
+
+// Esta función establece el juego de caracteres actual para el cliente
+mysqli_set_charset($conexion, "utf8");
+
+?>
