be1e3658d2fa44b658464ddd4c36a2088c33d0fb
Grigoriy <gregorct20@gmail.com>
2026-04-13 13:23:06 +0200

    El header genérico para reutilizarlo en mas páginas.(Con una barra de navegación)

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de Moda | Proyecto final DAW</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <header>
+        <nav class="barra_navegacion">
+            <div class="logo">MODA 2026</div>
+            <ul class="nav-links">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="vws/catalogo.php">Productos</a></li>
+                <li><a href="vws/contacto.php">Contacto</a></li>
+                <li><a href="vws/login.php" class="btn-login">Iniciar Sesión / Registro</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
